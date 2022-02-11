0c35ddbdc13bbf92af79376bd3b70124604c4c87
unknown <mukrinaman@gmail.com>
2022-02-11 11:27:32 +0530

    NMX project skeleton created

--- testcases.py
+++ testcases.py
@@ -1,14 +1,14 @@
-word1,word2="abcf","bca"        
-set1=set(word1)
-set2=set(word2)        
-freq1,freq2={},{}        
-for char in word1:
-    freq1[char]=freq1.get(char,0)+1
-        
-for char in word2:
-    freq2[char]=freq2.get(char,0)+1
-if sorted(freq1.values())==sorted(freq2.values()) and sorted(set1)==sorted(set2):
-    print(True)
-elif len(word1)!=len(word2):
-    print(False)
-else: print(False)
+def quick_sort(sequence):
+    length=len(sequence)
+    if length<=1:
+        return sequence
+    else:
+        pivot=sequence.pop()
+    items_greater,items_lower=[],[]
+    for item in sequence:
+        if item > pivot:
+            items_greater.append(item)
+        else:
+            items_lower.append(item)
+    return quick_sort(items_greater) + [pivot] + quick_sort(items_lower)
+print(quick_sort([25,85,89,110,11,223,56,65,89,1,2,5,20]))
